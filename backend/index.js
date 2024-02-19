48e5bd966bc4f8ad40f403928c23147c469cfd5c
sothulthorn <thornsothul@gmail.com>
2024-02-19 21:11:56 +0900

    Connect to the database

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+  console.log('Mongodb is connected');
+});
 
 const app = express();
 
