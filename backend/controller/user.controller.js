d6b3358c17b8710480306b331f2717c7e9c7db7b
sothulthorn <thornsothul@gmail.com>
2024-02-24 15:05:31 +0900

    Add delete user API route

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -53,5 +53,20 @@ export const updateUser = async (req, res, next) => {
 
     const { password, ...rest } = updateUser._doc;
     res.status(200).json(rest);
-  } catch (error) {}
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to delete this user'));
+  }
+
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json('User has been deleted');
+  } catch (error) {
+    next(error);
+  }
 };
